8f1bcedd75b53bc5b32d91131264a698b1bc9761
xbadboi17x <109177939+xbadboi17x@users.noreply.github.com>
2023-02-15 17:28:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-function test()
+var andrew_theme = new Audio('andrew.mp3');
+var errorHandel = 0;
+
+
+document.querySelector(".my-dp").addEventListener("click", function()
 {
-  if (check === 0)
+  if (errorHandel === 0)
   {
-    var andrew = new Audio("andrew.mp3");
-    document.querySelector(".my-dp").setAttribute("src", "https://i.guim.co.uk/img/media/59c1b14b1677cc33e27967cf6b11c8fd99a93761/0_105_1080_648/master/1080.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=655a8ae5af42857c3b3b576d8e922139");
-    andrew.play();
-    check += 1;
+    errorHandel = 1;
+    andrew_theme.play();
+    setTimeout(function()
+    {
+      document.querySelector(".my-dp").setAttribute("src", "https://media.tenor.com/wejEY_dJqOgAAAAd/walk-andrew-tate-walk.gif");
+    }, 2000);
+
+    setTimeout(function()
+    {
+      document.querySelector(".my-dp").setAttribute("src", "images/profile-pic.jpg");
+      errorHandel = 0;
+    }, 30000);
   }
-};
-document.querySelector(".my-dp").addEventListener("click", test);
-var check = 0;
+})
